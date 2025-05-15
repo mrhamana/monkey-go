8936ebdab1cbfd0aacc01cd7584fe5ab85293302
Saugat Khatiwada <saugatkhatiwada80@gmail.com>
2025-05-15 09:57:09 +0545

    sample code to test added and it WORKSSgit add .

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -24,18 +24,14 @@ func TestNextToken(t *testing.T) {
 
 	l := New(input)
 
-	for i , tt in := range tests {
-		tok:=l.NextToken()
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
 
-		if tok.Type!=tt.expectedType{
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q , got=%q",
-			i,tt.expectedType,tok.Type)
-		}
-		
-		if tok.Literal !=tt.expectedLiteral {
-			t.FatalF("tests[%d] - literal wrong. expected =%q,got=%q",
-			i,tt.expectedLiteral,tok.Literal)	
-		}
 	}
 }
-
