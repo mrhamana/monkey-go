6230340d2b61aa5f18f8d3c6413d704cebe15fe1
Saugat Khatiwada <saugatkhatiwada80@gmail.com>
2025-05-24 15:55:59 +0545

    made test script for parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -46,5 +46,19 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		t.Errorf("s.TokenLiteral() is not 'let' . Instead got =%q", s.TokenLiteral())
 		return false
 	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not let statement . got =%T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'.got %s", name, letStmt.Name.Value)
+		return false
+	}
 
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
 }
